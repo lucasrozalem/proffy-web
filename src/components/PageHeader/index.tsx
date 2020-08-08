06283ba63c980b0836fea049eca68665cfa30e1e
Lucas Rozalem <lrozalem@gmail.com>
2020-08-07 21:29:51 -0300

    componentes finalizados e estilizados

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -8,8 +8,8 @@ import './styles.css'
 
 interface PageHeaderProps {
   title: string;
+  description?: string; //nao é obrigatória
 }
-
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return (
@@ -23,6 +23,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
        <div className="header-content">
           <strong>{props.title}</strong>
+          { props.description && <p>{props.description}</p> }
 
           {props.children}
        </div>
